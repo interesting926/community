e0aaf54c1287e7e4d9c8339e241e959ed566b9ab
interesting926 <995941767@qq.com>
2020-10-13 21:37:00 +0800

    add github login

---
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -0,0 +1,51 @@
+package life.majiang.community.community.dto;
+
+public class GithubUser {
+    private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "login='" + login + '\'' +
+                ", name='" + name + '\'' +
+                ", id=" + id +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
+}
