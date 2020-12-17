51332e94b85f9c50d9e3a35df3146989b4bd6a3b
interesting926 <995941767@qq.com>
2020-12-17 10:02:12 +0800

    add test

--- src/main/java/life/majiang/community/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/community/service/QuestionService.java
@@ -45,6 +45,12 @@ public class QuestionService {
         QuestionExample example1 = new QuestionExample();
         long count = questionMapper.countByExample(example1);
 
+        Question question1 = new Question();
+        question1.setId((long)1);
+        question1.setViewCount(1);
+        questionExtMapper.incView(question1);
+
+
         PaginationDTO paginationDTO = new PaginationDTO();
 //        Integer totalCount = (int)questionMapper.countByExample(new QuestionExample());
         QuestionQueryDTO questionQueryDTO = new QuestionQueryDTO();
